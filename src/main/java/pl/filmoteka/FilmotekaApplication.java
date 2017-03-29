27ecdabb88b6a654bc44062abf2eb355c3c08fdf
Piotr Solarski <piotrsolarski@outlook.com>
2017-03-29 17:56:55 +0200

    initial commit

---
+++ src/main/java/pl/filmoteka/FilmotekaApplication.java
@@ -0,0 +1,12 @@
+package pl.filmoteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmotekaApplication.class, args);
+	}
+}
