b5ba5530e5e3a213ad86bce019f57117d8d46dc6
Piotr Solarski <piotrsolarski@outlook.com>
2017-03-29 21:29:46 +0200

    Dodano klase MyBean

---
+++ src/main/java/pl/filmoteka/Configuration/MyBean.java
@@ -0,0 +1,27 @@
+package pl.filmoteka.Configuration;
+
+/**
+ * Created by Piotr on 29.03.2017.
+ */
+public class MyBean {
+
+    private String beanName;
+
+    private String beanFunction;
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public String getBeanFunction() {
+        return beanFunction;
+    }
+
+    public void setBeanFunction(String beanFunction) {
+        this.beanFunction = beanFunction;
+    }
+}
