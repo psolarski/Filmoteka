ec94063ade909d9815c033f6ce2c183faead5f4d
Marek Świtalski <marek.switalski@outlook.com>
2017-03-29 18:18:59 +0200

    Zmodyfikowano pom.xml do HashBuilder oraz EqualsBuilder. Dodano klasę Director.

---
+++ src/main/java/pl/filmoteka/model/Director.java
@@ -0,0 +1,71 @@
+package pl.filmoteka.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.*;
+
+@Entity
+public class Director {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String nationality;
+
+    public Director() {}
+
+    public Director(final String name, final String surname, final String nationality) {
+        this.name = name;
+        this.surname = surname;
+        this.nationality = nationality;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+}
