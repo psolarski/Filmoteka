cd6c2de753e275fb36ec74f1f801ee706d7a018c
Piotr Solarski <piotrsolarski@outlook.com>
2017-04-05 14:49:44 +0200

    Uaktualniono projekt, potrzebne testy

---
+++ src/main/java/pl/filmoteka/Configuration/FilmotekaAutoConfig.java
@@ -0,0 +1,18 @@
+package pl.filmoteka.Configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Piotr on 05.04.2017.
+ */
+@Component
+public class FilmotekaAutoConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "bean.custom.mybean")
+    public MyBean createConfiguredBean() {
+        return new MyBean();
+    }
+}
---
+++ src/main/java/pl/filmoteka/controller/ActorsController.java
@@ -0,0 +1,42 @@
+package pl.filmoteka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.filmoteka.Configuration.MyBean;
+import pl.filmoteka.model.Actor;
+import pl.filmoteka.repository.ActorRepository;
+
+import java.util.List;
+
+/**
+ * Created by Piotr on 05.04.2017.
+ */
+@RestController
+@RequestMapping("api/v1/actors/")
+public class ActorsController {
+
+    @Autowired
+    private ActorRepository actorRepository;
+
+    @Autowired
+    private MyBean myBean;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Actor> findAll() {
+
+        System.out.println();
+        return actorRepository.findAll();
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public Actor createActor(@RequestBody Actor actor) {
+        Actor savedActor = actorRepository.saveAndFlush(actor);
+        return savedActor;
+    }
+
+    @RequestMapping(value = "testnum", method = RequestMethod.GET)
+    public void testActor(@RequestParam(value = "someint", required = false) Integer num) {
+
+        System.out.println(num);
+    }
+}
