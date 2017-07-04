848489cd1125bc26e8ae11d7f09a05db3f4b0381
Marek Świtalski <marek.switalski@outlook.com>
2017-07-04 22:58:26 +0200

    Dodano szkielet klasy użytkownika

---
+++ src/main/java/pl/filmoteka/model/User.java
@@ -0,0 +1,52 @@
+package pl.filmoteka.model;
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+
+/**
+ * Entity that represents an user.
+ *
+ * TODO: add roles and authorization
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
